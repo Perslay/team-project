4d4d2951af8774853f0a69d1fd61fa45fd609967
Perslay <124808493+Perslay@users.noreply.github.com>
2023-11-20 22:58:13 +0000

    Deploying to gh-pages from @ Perslay/team-project@52f329e03c901c423ac853eaceba4c3d74ba8ff5 🚀

--- index.57c9f318.js
+++
@@ -1,3 +0,0 @@
-//# sourceMappingURL=index.57c9f318.js.map
-
-//# sourceMappingURL=index.57c9f318.js.map
---
+++ index.d8858117.js
@@ -0,0 +1,3 @@
+(()=>{let e={openReviewBtn:document.querySelector("[data-modal-review-open]"),closeReviewBtn:document.querySelector("[data-modal-review-close]"),modal:document.querySelector("[data-modal-review]")};function t(){e.modal.classList.toggle("is-hidden")}e.openReviewBtn.addEventListener("click",t),e.closeReviewBtn.addEventListener("click",t)})();//# sourceMappingURL=index.d8858117.js.map
+
+//# sourceMappingURL=index.d8858117.js.map
