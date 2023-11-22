974e813a4cf9e617128302f6535dbf57d1ef126b
Perslay <124808493+Perslay@users.noreply.github.com>
2023-11-22 18:50:54 +0000

    Deploying to gh-pages from @ Perslay/team-project@2634cf82efebd55425dc23833980d8c856f13c45 🚀

---
+++ index.6fe7f8ff.js
@@ -0,0 +1,3 @@
+new Swiper(".products-js-slider",{slidesPerView:4,spaceBetween:18,pagination:{el:".products-js-pagination",clickable:!0},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:2,spaceBetween:18},1200:{slidesPerView:4,spaceBetween:18}}});//# sourceMappingURL=index.6fe7f8ff.js.map
+
+//# sourceMappingURL=index.6fe7f8ff.js.map
