81d3d73467164998a54bff8acae8a646281deb40
Perslay <124808493+Perslay@users.noreply.github.com>
2023-11-24 17:06:45 +0000

    Deploying to gh-pages from @ Perslay/team-project@5f4f22f5de27c1c550742a5401975d4bcf71abdb 🚀

---
+++ index.910b5659.js
@@ -0,0 +1,3 @@
+(()=>{let e={openBuyBtn:document.querySelector("[data-modal-buy-open]"),openBuyBtnTwo:document.querySelector("[data-modal-buy-open-sellers]"),closeBuyBtn:document.querySelector("[data-modal-buy-close]"),modal:document.querySelector("[data-modal-buy]")};function t(){e.modal.classList.toggle("is-hidden")}e.openBuyBtn.addEventListener("click",t),e.openBuyBtnTwo.addEventListener("click",t),e.closeBuyBtn.addEventListener("click",t)})();//# sourceMappingURL=index.910b5659.js.map
+
+//# sourceMappingURL=index.910b5659.js.map
--- index.d2c69b34.js
+++
@@ -1,3 +0,0 @@
-(()=>{let e={openBuyBtn:document.querySelector("[data-modal-buy-open]"),closeBuyBtn:document.querySelector("[data-modal-buy-close]"),modal:document.querySelector("[data-modal-buy]")};function t(){e.modal.classList.toggle("is-hidden")}e.openBuyBtn.addEventListener("click",t),e.closeBuyBtn.addEventListener("click",t)})();//# sourceMappingURL=index.d2c69b34.js.map
-
-//# sourceMappingURL=index.d2c69b34.js.map
